775e27f237d943d73828ded328cee5c7b108290e
Ali Arslan <ali_arslan@brown.edu>
2013-07-16 11:43:38 -0400

    started adding bfast code

--- classificationGeneralCodebase/convert_mat_to_bigtable_bfast.py
+++ classificationGeneralCodebase/convert_mat_to_bigtable_bfast.py
@@ -44,12 +44,13 @@ def read_mat_files(features_basename, labels_fname, camname_fname, actname_fname
     tic = time.time()
     
     f = h5py.File(features_basename + '_part1.mat', 'r')
-    ff = f["myData"]
-    print ff.shape
-    print ff.chunks
-    print ff.compression
-    print ff.compression_opts
     import ipdb; ipdb.set_trace()
+    ff = f["myData"]
+#    print ff.shape
+#    print ff.chunks
+#    print ff.compression
+#    print ff.compression_opts
+#    import ipdb; ipdb.set_trace()
     features = sp.array(ff).T
     for nn in range(2,N_PARTS+1):
         f = h5py.File(features_basename + '_part' + str(nn)+ '.mat', 'r')
